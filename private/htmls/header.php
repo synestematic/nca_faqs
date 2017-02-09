47ed7d7e22f54ed988e5bb90fec2783a8460eee3
Fed Rizzo <synestematic@gmail.com>
2017-02-09 11:30:38 +0100

    - Removes unused public.css and link from header

--- private/htmls/header.php
+++ private/htmls/header.php
@@ -8,7 +8,6 @@
  	<meta name="viewport" content="width=device-width, initial-scale=1">
 
  	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,800' rel='stylesheet' type='text/css'>
-   <!-- <link rel="stylesheet" href="css/public.css" media="all" type="text/css" /> -->
  	<link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
  	<link rel="stylesheet" href="css/style.css"> <!-- Resource style -->
  	<script src="js/modernizr.js"></script> <!-- Modernizr -->
